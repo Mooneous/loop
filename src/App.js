1b194d24096690afedaf10cf19b345cadf483830
Mooneous <4087022@naver.com>
2022-08-29 15:22:41 +0900

    좌우버튼 이벤트 발생시 활성해 패널 순번 구하기

--- src/App.js
+++ src/App.js
@@ -14,20 +14,27 @@ function App() {
 		{ title: 'WINDY', subTitle: 'MOUNTAIN', pic: 'pic5.jpg' },
 		{ title: 'DIMMED', subTitle: 'LANDSCAPE', pic: 'pic6.jpg' },
 		{ title: 'SUNSET', subTitle: 'GRASSLAND', pic: 'pic7.jpg' },
-		//{ title: 'LAPPING', subTitle: 'SEA WAVES', pic: 'pic8.jpg' },
+		// { title: 'LAPPING', subTitle: 'SEA WAVES', pic: 'pic8.jpg' },
 	];
+	let [Active, setActive] = useState(0);
 	const path = process.env.PUBLIC_URL;
 	const list = useRef(null);
 
 	const prev = () => {
 		const lastEl = list.current.lastElementChild;
 		list.current.prepend(lastEl);
+		setActive(Active === 0 ? (Active = 7 - 1) : --Active);
 	};
 
 	const next = () => {
 		const firstEl = list.current.firstElementChild;
 		list.current.append(firstEl);
+		setActive((Active) => (Active === 7 - 1 ? (Active = 0) : ++Active));
 	};
+
+	useEffect(() => {
+		console.log(Active);
+	}, [Active]);
 
 	return (
 		<main>
